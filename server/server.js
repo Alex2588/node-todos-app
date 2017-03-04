c6b5daca9ea8322ae9362248cddb18dad911fa0d
Alex2508git config --global user.name Alex2508 <alexander_beschasnyy@mail.ru>
2017-03-04 19:37:48 +0200

    Add GET todos/id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var { ObjectID } = require('mongodb');
 
 var app = express();
 
@@ -26,6 +27,24 @@ app.get('/todos', (req, res) => {
         res.send({todos});
     }, (err) => {
         res.status(400).send(err);
+    });
+});
+
+app.get('todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(!Object.valid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        };
+
+        res.status(200).send({todo});
+    },(e) => {
+        res.status(400).send();
     });
 });
 
